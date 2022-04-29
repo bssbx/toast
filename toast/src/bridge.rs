165c546ec4cffa30c7b068c9a22c87ab218c5f28
codck <104614354+bsubx@users.noreply.github.com>
2022-04-29 14:45:18 +0800

    update

---
+++ toast/src/bridge.rs
@@ -0,0 +1,3 @@
+extern "C" fn send_packet(packet: String) {
+    println!("yeah, the kernel got the packet data");
+}
--- toast/src/main.rs
+++ toast/src/main.rs
@@ -1,3 +1,9 @@
+mod api;
+mod bridge;
+mod db;
+mod node;
+mod user;
+
 fn main() {
     println!("Hello, world!");
 }
